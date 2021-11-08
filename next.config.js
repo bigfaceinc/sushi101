6083468d937b1b0a1702eec7b6519b3a87fc39e2
Jun Guo <gjuoun@gmail.com>
2021-11-08 04:50:10 -0400

    no necessary assetPrefix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const withOptimizedImages = require('next-optimized-images');
 
 /** @type {import('next').NextConfig} */
 module.exports = withOptimizedImages({
-  assetPrefix: 'https://www.sushi101.ca',
   reactStrictMode: true,
   handleImages: ['jpeg', 'png'],
 })
