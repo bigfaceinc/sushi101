0e91f8425712f48f79051980fd720b00ac05195b
Jun Guo <gjuoun@gmail.com>
2021-11-08 04:40:41 -0400

    upload assets

---
+++ docs/_next/static/aqpoJIvnFDCmOA-yk4fuI/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Ftakeout","\u002Fmenu","\u002Flunch"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withOptimizedImages = require('next-optimized-images');
 
 /** @type {import('next').NextConfig} */
 module.exports = withOptimizedImages({
+  assetPrefix: 'https://www.sushi101.ca',
   reactStrictMode: true,
   handleImages: ['jpeg', 'png'],
 })
