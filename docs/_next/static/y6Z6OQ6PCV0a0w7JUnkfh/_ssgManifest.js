4d9205426309b261dd9ebf14cf7cdfd3508f7e34
Jun Guo <gjuoun@gmail.com>
2021-11-10 18:14:18 +0000

    update favicon

---
+++ docs/_next/static/y6Z6OQ6PCV0a0w7JUnkfh/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Flunch","\u002Ftakeout","\u002Fmenu"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,13 +19,13 @@ function MyApp({ Component, pageProps }: AppProps) {
       <meta name="description" content={siteInfo.description} />
       <meta name="keywords" content={siteInfo.keywords} />
       <meta property="og:title" content={siteInfo.title} key="title" />
-      <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-      <meta name="msapplication-TileColor" content="#da532c" />
-      <meta name="theme-color" content="#ffffff"></meta>
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="theme-color" content="#ffffff" />
       <link rel="icon" href="/favicon.ico"></link>
     </Head>
     <ThemeProvider theme={sushi101Theme} >
